34cc1794b42279034a07adba0807f848ad2b3cfb
Kirill Chaykin <k.chaykin@metamodel.ru>
2023-10-21 18:43:55 +0500

    Add *_DELETED support

--- src/main/java/ru/chaykin/wjss/action/ChangeTypeActionFactory.java
+++ src/main/java/ru/chaykin/wjss/action/ChangeTypeActionFactory.java
@@ -2,18 +2,22 @@ package ru.chaykin.wjss.action;
 
 import java.util.Map;
 
+import ru.chaykin.wjss.action.impl.LocalDeletedChangeTypeAction;
 import ru.chaykin.wjss.action.impl.LocalUpdatedChangeTypeAction;
+import ru.chaykin.wjss.action.impl.RemoteDeletedChangeTypeAction;
 import ru.chaykin.wjss.action.impl.RemoteNewChangeTypeAction;
 import ru.chaykin.wjss.action.impl.RemoteUpdatedChangeTypeAction;
 import ru.chaykin.wjss.calc.ChangeType;
 
+import static ru.chaykin.wjss.calc.ChangeType.*;
+
 public class ChangeTypeActionFactory {
     private static final Map<ChangeType, IChangeTypeAction> ACTIONS = Map.of(
-		    ChangeType.REMOTE_NEW, new RemoteNewChangeTypeAction(),
-		    ChangeType.REMOTE_UPDATED, new RemoteUpdatedChangeTypeAction(),
-		    //ChangeType.REMOTE_DELETED, new RemoteDeletedChangeTypeAction(),
-		    ChangeType.LOCAL_UPDATED, new LocalUpdatedChangeTypeAction()/*,
-		    ChangeType.LOCAL_DELETED, new LocalDeletedChangeTypeAction()*/);
+		    REMOTE_NEW, new RemoteNewChangeTypeAction(),
+		    REMOTE_UPDATED, new RemoteUpdatedChangeTypeAction(),
+		    REMOTE_DELETED, new RemoteDeletedChangeTypeAction(),
+		    LOCAL_UPDATED, new LocalUpdatedChangeTypeAction(),
+		    LOCAL_DELETED, new LocalDeletedChangeTypeAction());
 
     public static IChangeTypeAction create(ChangeType changeType) {
 	return ACTIONS.get(changeType);
---
+++ src/main/java/ru/chaykin/wjss/graphql/mutation/DeletePageMutation.java
@@ -0,0 +1,40 @@
+package ru.chaykin.wjss.graphql.mutation;
+
+import lombok.RequiredArgsConstructor;
+import ru.chaykin.wjss.data.IPage;
+import ru.chaykin.wjss.graphql.api.ClientApi;
+
+@RequiredArgsConstructor
+public class DeletePageMutation {
+    private static final String UPDATE_MUTATION = """
+		    mutation {
+		    	pages {
+		    		delete(id: %s) {
+		    			responseResult { succeeded errorCode slug message }
+		    		}
+		    	}
+		    }""";
+
+    private final ClientApi api;
+
+    public void deletePage(IPage page) {
+	String mutation = String.format(UPDATE_MUTATION, page.getId());
+
+	//noinspection uncheckeds
+	var result = api.mutation(Type.class, mutation).data().pages().delete().responseResult();
+	if (!result.succeeded()) {
+	    throw new RuntimeException("Could not delete page " + page.getId() + ". Response: " + result);
+	}
+    }
+
+    private record Type(Data data) {
+	private record Data(Pages pages) {
+	    private record Pages(Delete delete) {
+		private record Delete(ResponseResult responseResult) {
+		    private record ResponseResult(boolean succeeded, int errorCode, String slug, String message) {
+		    }
+		}
+	    }
+	}
+    }
+}
